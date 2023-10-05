898ebfd859f6215166648a4b0e73e02dec8b5a06
Bhavya-Goyal423 <Goel2000bhavya@gmail.com>
2023-10-05 19:30:00 +0530

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Hero from "./components/Hero/Hero.component";
+import Navbar from "./components/Navbar/Navbar.component";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Hero />
     </>
   );
---
+++ src/components/Navbar/Navbar.component.jsx
@@ -0,0 +1,68 @@
+import "./Navbar.styles.css";
+
+export default function Navbar() {
+  return (
+    <header className="header">
+      <img src="/img/omnifood-logo.png" alt="Omnifood logo" className="logo" />
+      <nav className="main-nav">
+        <ul className="main-nav-list">
+          <li>
+            <a
+              className="main-nav-link"
+              href="/"
+              onClick={(e) => {
+                e.preventDefault();
+              }}
+            >
+              Section 1
+            </a>
+          </li>
+          <li>
+            <a
+              className="main-nav-link"
+              href="/"
+              onClick={(e) => {
+                e.preventDefault();
+              }}
+            >
+              Section 2
+            </a>
+          </li>
+          <li>
+            <a
+              className="main-nav-link"
+              href="/"
+              onClick={(e) => {
+                e.preventDefault();
+              }}
+            >
+              Section 3
+            </a>
+          </li>
+          <li>
+            <a
+              className="main-nav-link"
+              href="/"
+              onClick={(e) => {
+                e.preventDefault();
+              }}
+            >
+              Section 4
+            </a>
+          </li>
+          <li>
+            <a
+              className="main-nav-link nav-cta"
+              href="/"
+              onClick={(e) => {
+                e.preventDefault();
+              }}
+            >
+              Section 5
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
